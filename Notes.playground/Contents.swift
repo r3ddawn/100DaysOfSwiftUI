78da7db00d59654675d6bacbfd67e0cf8ae7f669
Eric <ejohnson1028@gmail.com>
2023-02-05 21:56:02 -0800

    Day 10 notes
    Finished day 10. Learned about Structs, initializers, computed properties, and property observers

--- Notes.playground/Contents.swift
+++ Notes.playground/Contents.swift
@@ -609,7 +609,7 @@ do {
 
 
 
-
+/*
 // Closures
 func greetUser() {
     print("Hi there!")
@@ -705,4 +705,124 @@ doImportantWork {
 } third: {
     print("This is the third work")
 }
- 
+*/
+
+
+
+
+// Structs
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+    
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
+    }
+}
+
+let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
+let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+print(red.title)
+print(wings.artist)
+
+red.printSummary()
+wings.printSummary()
+
+struct Employee {
+    let name: String
+    var sickDays: Int
+    var vacationRemaining: Int = 14    //Set default to 14
+    
+    mutating func takeVacation(days: Int) {
+        if vacationRemaining > days {
+            vacationRemaining -= days
+            print("I'm going on vacation")
+            print("Days remaining: \(vacationRemaining)")
+        } else {
+            print("Oops! There aren't enough days remaining.")
+        }
+    }
+}
+
+var archer = Employee(name: "Sterling Archer", sickDays: 10, vacationRemaining: 14)
+archer.takeVacation(days: 5)
+print(archer.vacationRemaining)
+
+let a = 1
+let b = 2.0
+let c = Double(a) + b
+
+// Computed properties - Getters - Setters
+struct EmployeeCalculated {
+    let name: String
+    var vacationAllocated = 14
+    var vacationTaken = 0
+    
+    var vacationRemaining: Int {
+        get {
+            return vacationAllocated - vacationTaken
+        }
+        
+        set {
+            return vacationAllocated = vacationTaken + newValue
+        }
+    }
+}
+
+var demi = EmployeeCalculated(name: "Demi", vacationAllocated: 24)
+demi.vacationTaken += 4
+demi.vacationRemaining = 5
+print(demi.vacationRemaining)
+
+//Property Observers
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+        
+        didSet {
+            print("There are now \(contacts.count)")
+            print("Old value was: \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Eric J.")
+app.contacts.append("Karissa J.")
+app.contacts.append("Deacon J.")
+app.contacts.append("Piper J.")
+
+
+// Initializers
+struct Player {
+    let name: String
+    let number: Int
+
+    init(name: String) {
+        self.name = name
+        //self.number = number
+        number = Int.random(in: 1...99)
+    }
+}
+
+let player = Player(name: "Megan R")
+print(player)
