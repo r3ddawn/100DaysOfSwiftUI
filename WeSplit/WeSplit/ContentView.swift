041495f9dcb451bc0c571312aea05495b756b7ad
Eric <ejohnson1028@gmail.com>
2023-02-11 11:48:04 -0800

    Day 16 - Created WeSplit project
    Created app files and went over basics surrounding views, manipulating text input, and saving that input back to variables.

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Eric Johnson on 2/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
