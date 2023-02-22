d07d2e9487d3a9d8a9734d9ac42a492f3f4c8b6a
Eric <ejohnson1028@gmail.com>
2023-02-22 14:09:36 -0800

    Day 26 - BetterRest started
    Reviewed new concepts we will use to make next app

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Eric Johnson on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
