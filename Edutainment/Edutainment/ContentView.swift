6708b15456108685ddeff3f3d328776f2b43f7d7
Eric <ejohnson1028@gmail.com>
2023-02-26 21:31:18 -0800

    Day 35 Complete
    Completed the milestone project for day 35

--- Edutainment/Edutainment/ContentView.swift
+++ Edutainment/Edutainment/ContentView.swift
@@ -7,70 +7,172 @@
 
 import SwiftUI
 
+struct GenerateQuestionLists {
+    var tableMax: Int
+    private var questionAnswerList: [[Int]] {
+        var list = [[Int]]()
+        
+        for x in 1...12 {
+            for n in 1...tableMax {
+                list.append([x, n, x * n])
+            }
+        }
+        return list
+    }
+    
+    func getQuestion(_ num: Int) -> String {
+        "\(questionAnswerList[num][0]) x \(questionAnswerList[num][1]) = "
+        
+    }
+    
+    func getAnswer(_ num: Int) -> Int {
+        questionAnswerList[num][2]
+    }
+    
+    func count() -> Int {
+        questionAnswerList.count
+    }
+}
+
 struct ContentView: View {
-    @State private var multiplicationTable = 6
-    @State private var answers = [Int]()
+    @State private var currentQuestion = ""
+    @State private var currentAnswer = 0
+    @State private var answeredQuestions = [String]()
+    @State private var questionCount = 0
+    
+    @State private var numOfQuestionOptions = [5, 10, 20]
+    @State private var numOfQuestionPicked = 10
+    @State private var tableMaximum = 10
+    @State private var userAnswer = ""
+    @State private var userScore = 0
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    @State private var showingSettings = true
     
     var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors:[.black, .purple, .teal, .white]), startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea()
-            Color.secondary.ignoresSafeArea()
-            VStack {
-                Text("What multiplication tables would you like to see?")
-                    .foregroundColor(.white)
-                HStack {
-                    Spacer()
-                    
-                    Button {
-                        if multiplicationTable > 2 {
-                            multiplicationTable -= 1
-                        }
-                    } label: {
-                        Image(systemName: "minus")
-                            .redMinusButton()
-                    }
-                    
-                    Spacer()
-                    
-                    Text("\(multiplicationTable)")
-                        .font(.title)
-                        .foregroundColor(.white)
-                    
-                    Spacer()
-                    
-                    Button {
-                        if multiplicationTable < 12 {
-                            multiplicationTable += 1
-                        }
-                    } label: {
-                        Image(systemName: "plus")
-                            .greenPlusButton()
-                    }
-                    
-                    ForEach(0..<3) { _ in
-                        Spacer()
-                    }
-                    
-                    Button {
-                        //More to come
-                    } label: {
-                        Text("Select")
-                            .selectButton()
-                    }
-                    
-                    Spacer()
-                } // Custom stepper
-                
+            NavigationView {
                 VStack {
-                    ForEach(1..<(12 + 1)) { num in
+                    Text("")
+                        .navigationTitle("Edutainment")
+                    
+                    Section {
+                        if showingSettings {
+                            Text("Setup")
+                                .font(.headline)
+                            Stepper("Tables up to:    \(tableMaximum)", value: $tableMaximum, in: 2...12)
+                                .font(.headline)
+                            Picker("Number of Questions", selection: $numOfQuestionPicked) {
+                                ForEach(numOfQuestionOptions, id: \.self) {
+                                    Text("\($0)")
+                                }
+                            }
+                            .pickerStyle(.segmented)
+                            Button {
+                                showingSettings.toggle()
+                            } label: {
+                                Text("Save")
+                            }
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(width: 100, height: 25)
+                            .background(LinearGradient(gradient: Gradient(colors: [.teal, .blue, .teal]), startPoint: .top, endPoint: .bottom))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                        }
+                    } // Game Settings
+                    
+                    Section {
                         HStack {
-                            Text("\(num) x \(multiplicationTable) = ")
+                            
+                            Spacer()
+                            
+                            Text(currentQuestion)
+                                .font(.title)
+                            TextField("Answer", text: $userAnswer)
+                                .fixedSize()
+                                .textFieldStyle(.roundedBorder)
+                                .border(Color.blue)
+                                .keyboardType(.numberPad)
+                            Button {
+                                submitAnswer()
+                                if showingSettings { showingSettings.toggle() }
+                            } label: {
+                                Text("Submit")
+                            }
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(width: 75, height: 25)
+                            .background(LinearGradient(gradient: Gradient(colors: [.teal, .blue, .teal]), startPoint: .top, endPoint: .bottom))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            
+                            Spacer()
+                        }
+                        .padding(50)
+                        
+                        ForEach(answeredQuestions, id: \.self) { answer in
+                            Text(answer)
                                 .font(.title)
                         }
+                        
+                        Spacer()
+                    }
+                }
+                .navigationBarTitleDisplayMode(.automatic)
+                .toolbar {
+                    Button {
+                        showingSettings.toggle()
+                    } label: {
+                        Text(showingSettings ? "Hide Setup" : "Show Setup")
                     }
                 }
             }
+            
+            .onAppear(perform: startGame)
+            .onSubmit(submitAnswer)
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(alertMessage)
+            }
+    }
+    
+//  Make it go Vroom
+    func startGame() {
+        let list = GenerateQuestionLists(tableMax: tableMaximum)
+        let num = Int.random(in: 0...list.count())
+        
+        currentQuestion = list.getQuestion(num)
+        currentAnswer = list.getAnswer(num)
+        userAnswer = ""
+    }
+    
+    func checkAnswer() -> Bool {
+        currentAnswer == Int(userAnswer) ?? 0
+    }
+    
+    func submitAnswer() {
+        questionCount += 1
+        if checkAnswer() { userScore += 1 }
+        let image = checkAnswer() ? "   ✅" : "   ❌"
+        answeredQuestions.append (currentQuestion + userAnswer + image)
+        if questionCount >= numOfQuestionPicked {
+            alertTitle = "Game Over"
+            alertMessage = "Your Score: \(userScore) / \(numOfQuestionPicked)"
+            showingAlert.toggle()
+            completeReset()
+        } else {
+            startGame()
         }
+    }
+    
+    func completeReset() {
+        startGame()
+        answeredQuestions = [String]()
+        userScore = 0
+        questionCount = 0
+        showingSettings.toggle()
     }
 }
 
