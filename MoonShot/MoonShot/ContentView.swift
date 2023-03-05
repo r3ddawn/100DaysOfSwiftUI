ff3161608a5bbfb7d3f72f49cd7605f25fdf6362
Eric <ejohnson1028@gmail.com>
2023-03-04 16:08:36 -0800

    Day 39 Complete
    Created App File and reviewed basic concepts we will use to build MoonShot

---
+++ MoonShot/MoonShot/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MoonShot
+//
+//  Created by Eric Johnson on 3/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoonShot/MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Eric Johnson on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
