6abd395884a3d60ed6e24e74a9e471a0327266aa
Eric <ejohnson1028@gmail.com>
2023-03-05 20:05:30 -0800

    Day 43 Init Commit
    Day 43 Drawing app files created

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Eric Johnson on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
