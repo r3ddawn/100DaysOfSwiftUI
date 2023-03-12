094abbfdaa15a8e93d8fb4ed6e14ebb374bf1001
Eric <ejohnson1028@gmail.com>
2023-03-11 19:18:12 -0800

    Day 46 complete
    Completed part 1 of the challenge for today.

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,9 +7,34 @@
 
 import SwiftUI
 
+struct Arrow: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.minX, y: rect.maxY / 3))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY / 3))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY / 3))
+        path.move(to: CGPoint(x: rect.midX - 10, y: rect.maxY/3))
+        path.addLine(to: CGPoint(x: rect.midX - 10, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX + 10, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX + 10, y: rect.maxY / 3))
+
+        return path
+    }
+}
+
 struct ContentView: View {
+    @State private var lineThickness = 2.0
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Arrow()
+                .stroke(.red, style: StrokeStyle(lineWidth: lineThickness, lineCap: .round, lineJoin: .round))
+                .frame(width: 100, height: 300)
+            Slider(value: $lineThickness, in: 0...10)
+            Text("Current Line Weight: \(Int(lineThickness))")
+        }
     }
 }
 
