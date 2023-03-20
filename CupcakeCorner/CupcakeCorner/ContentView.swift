94b47827a8a3e895fa763a7fbad1ef3711c59cd2
Eric <ejohnson1028@gmail.com>
2023-03-20 16:39:26 -0700

    Day 49 Complete
    Reviewed all topics for day 49

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,27 +7,9 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
-    
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }
+        Text("Hello, World!")
     }
 }
 
