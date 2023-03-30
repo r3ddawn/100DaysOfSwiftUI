8f0f248b277a9f0f9b8df65d1607575af45ce296
Eric <ejohnson1028@gmail.com>
2023-03-29 19:32:44 -0700

    Created Bookworm app file
    Created project for day 53.

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Eric Johnson on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
